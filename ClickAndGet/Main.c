c0b2ba7bd1e3df8547ccffdec6db393d35f1410a
Zacch01 <zaccharieattias@gmail.com>
2021-11-13 17:08:01 +0200

    test use exel

--- ClickAndGet/Main.c
+++ ClickAndGet/Main.c
@@ -1,7 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+typedef struct manager
+{
+	char last_name[25];
+	char first_name[25];
+	char user_name[25];
+	long phone;
+	int age;
+	char password[20];
+};
+
+
 
 int main()
 {
-	printf("test MODIFIED");
+	FILE* fic = fopen("TestProjet.csv", "r+");
+	if (fic == NULL)
+		exit(1);
+
+	int name=0, age=0;
+	fscanf(fic, "%d;%d", &name, &age);
+	printf("%d %d", name, age);
+	name = 77;
+	age = 66;
+	fprintf(fic, ";%d;%d;987\n44; ; ; ;88",name,age);
+
+	fclose(fic);
 	return 0;
 }
